d0931ab1f67f4baab0d32d3bd0ea48ee743e6c71
Abhinav Mishra <abhinav.thinktank@gmail.com>
2016-09-14 21:13:11 +0530

    Added bubble sort and selection sort

--- src/com/Thinktank/Main.java
+++ src/com/Thinktank/Main.java
@@ -6,13 +6,19 @@ public class Main {
 static int[] unsortedArray = {3,1,9,10,12,8,4,6};
     public static void main(String[] args) {
         Main.insertionSort(unsortedArray);
+        Main.selectionSort(unsortedArray);
+        int arr[] = {1,2,3,4};
+        Main.bubbleSort(arr);
 
     }
 
 
-/*Insertion sort is a sorting algorithm which operates by dividing the array into a sorted and an unsorted half,
+/*
+INSERTION SORT
+Insertion sort is a sorting algorithm which operates by dividing the array into a sorted and an unsorted half,
 By choosing an element from the unsorted half and finding it's place in the sorted half, the algorithm sorts
 the given array.
+
 Time complexity : Best Case(Array is already sorted then the inner while loop doesn't execute , Time complexity is O(n)
                   Worst Case(Array is reverse sorted) inner while loop executes the same number of times as array length,
                   therefore O(n^2)
@@ -43,4 +49,50 @@ Time complexity : Best Case(Array is already sorted then the inner while loop do
 
         System.out.println(Arrays.toString(arr));
     }
+
+
+    /*SELECTION SORT*/
+    public static void selectionSort(int arr[])
+    {
+        int minIndex,temp;
+        for(int i =0;i<arr.length-1;i++)
+        {
+             minIndex = i;
+            for(int j = i+1;j<arr.length;j++)
+            {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+            temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+/*BUBBLE SORT*/
+    public static void bubbleSort(int arr[])
+    {
+        int temp;
+        boolean didSwap = false;
+        for (int i =0;i<arr.length;i++)
+        {
+            didSwap = false;
+            for (int j =0;j<arr.length-i-1;j++)
+                if(arr[j]>arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    didSwap=true;
+                }
+                if(!didSwap)
+                    break;
+        }
+        System.out.println(Arrays.toString(arr)+didSwap);
+    }
+
+
+
 }
